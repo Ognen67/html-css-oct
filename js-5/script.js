d82f0a7a2b60747c266658e65cdc24733bd21dda
Ognen Kirov Todorovski <todorovskiognen@gmail.com>
2025-11-17 22:10:08 +0100

    Update DOM

--- js-5/script.js
+++ js-5/script.js
@@ -16,7 +16,15 @@ console.log(3);
 
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((res) => res.json())
-  .then((res) => console.log(res))
+  .then((res) => {
+    console.log(res);
+    const todosContainer = document.getElementById("todos");
+
+    res.forEach(
+      (todo, index) =>
+        (todosContainer.innerHTML += `<p class="todo ${todo.completed === true ? "" : "red"}">${index}: ${todo.title}</p>`)
+    );
+  })
   .catch((error) => console.log("Nastana greska: ", error))
   .finally(() => console.log("Baranjeto zavrsi!"));
 
@@ -61,8 +69,30 @@ console.log(buttonElement);
 const darkModeToggle = document.getElementById("dark-mode");
 
 darkModeToggle.addEventListener("click", () => {
+// confirm("Dark mode!")
   document.body.classList.toggle("dark-mode");
   darkModeToggle.classList.toggle("dark-toggle");
   console.log(darkModeToggle);
 });
 
+const contentElements = document.getElementsByClassName("content");
+
+for (let el of contentElements) {
+  console.log(el);
+}
+
+Array.from(contentElements).forEach((el) => console.log(el));
+
+
+// Homework
+
+// menuvanje na stil na elementi so ista klasa
+// site p elementi menuvanje fontFamily preku style svojstvoto
+// Event Listeners ("click", "mouseover", "mouseleave") create alert() popup
+  // confirm popup for conditional dark mode
+// fetch + DOM rendering na lista na objekti (public API)
+// Error handling so catch
+// setTimeout + setInterval
+
+// Input pole preku koe mozam da kreiram todo so svojstva: "title" i "completed"
+// click listener na sekoe todo, toggle completed + hover + cursor: pointer
